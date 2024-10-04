4fe12c1da9779c464c47d537e7e46e3ab9da53c8
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-10-04 15:18:10 +0900

    [level 0] Title: 나이 출력, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.py
@@ -0,0 +1,10 @@
+def solution(age):
+    return  2022 - (age - 1)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
