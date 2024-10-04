acbb75c391dc7f7be9abc66d00bcfa8d502200c6
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-10-04 14:49:21 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,6 +1,13 @@
 def solution(num1, num2):
     
-    if num1 == num2:
-        return 1
-    else:
+    # if num1 == num2:
+    #     return 1
+    # else:
+    #     return -1
+    
+    answer = 1
+    
+    if num1 != num2:
         return -1
+    
+    return answer
