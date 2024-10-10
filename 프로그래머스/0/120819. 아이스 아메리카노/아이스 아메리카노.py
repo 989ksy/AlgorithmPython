e3b87280d5ed83309438ec8a14ca4770c4c01a70
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-10-10 17:11:12 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,15 +1,4 @@
 def solution(money):
-    answer = []
-    c = 0
-    m = 0
-
-    if money - (5500 *(money // 5500)) >= 0:
-        
-        c = money // 5500
-        m = money - (5500 * c)
-        
-        answer.append(c)
-        answer.append(m)
-        
-        return answer
+    answer = [money // 5500, money % 5500]
+    return answer
         
