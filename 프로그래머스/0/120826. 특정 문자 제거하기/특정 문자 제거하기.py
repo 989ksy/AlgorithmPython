56c89fa4b3b5e53c6dbef6650de69e47b4e37206
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-10-11 10:25:52 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,10 +1,8 @@
 def solution(my_string, letter):
-    # 1. string to list
-    new_list = list(my_string)
+    answer = ''
     
-    # 2. Remove item
-    while letter in new_list:
-        new_list.remove(letter)
-        
-    # 3. list to String
-    return "".join(new_list)
+    for i in my_string:
+        if i != letter:
+            answer += i
+    
+    return answer
