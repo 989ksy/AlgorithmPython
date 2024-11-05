47f4a797ec149c997f9be87ea5eb0e0b12b93843
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-11-05 14:31:39 +0900

    [level 1] Title: 소수 찾기, Time: 3592.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,21 +1,41 @@
+# import math
+
+# def solution(n):
+#     answer = 0
+#     arr = []
+    
+#     arr = [True for i in range(n+1)]
+    
+#     for i in range(2, int(math.sqrt(n))+1):
+#         if arr[i] == True:
+#             j = 2
+            
+#             while i * j <= n:
+#                 arr[i*j] = False
+#                 j += 1
+    
+#     for i in range(2, n+1):
+#         if arr[i] == True:
+#             answer += 1
+    
+#     return answer
+
 import math
 
+def is_prime(a):
+    if a == 1:
+        return 0
+    for i in range(2, int(math.sqrt(a))+1):
+        if a % i == 0:
+            return 0
+    return 1
+
 def solution(n):
-    answer = 0
-    arr = []
     
-    arr = [True for i in range(n+1)]
-    
-    for i in range(2, int(math.sqrt(n))+1):
-        if arr[i] == True:
-            j = 2
-            
-            while i * j <= n:
-                arr[i*j] = False
-                j += 1
+    cnt = 0
     
     for i in range(2, n+1):
-        if arr[i] == True:
-            answer += 1
-    
-    return answer
+        if is_prime(i):
+            cnt += 1
+
+    return cnt
